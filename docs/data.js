faa6a27457e84e0eda9011fdbc9b7686bc961c15
github-action-benchmark <github@users.noreply.github.com>
2022-07-20 08:20:58 +0000

    add TZ Library Benchmark (pytest) benchmark result for a101444fdd04a31885a6a9ea2a85608e5d541617

---
+++ docs/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1658305258360,
+  "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
+  "entries": {
+    "TZ Library Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "ringsaturn.me@gmail.com",
+            "name": "ringsaturn",
+            "username": "ringsaturn"
+          },
+          "committer": {
+            "email": "ringsaturn.me@gmail.com",
+            "name": "ringsaturn",
+            "username": "ringsaturn"
+          },
+          "distinct": true,
+          "id": "a101444fdd04a31885a6a9ea2a85608e5d541617",
+          "message": "trigger ci",
+          "timestamp": "2022-07-20T16:20:23+08:00",
+          "tree_id": "bffc5c3f0fcf929946ba606662e3112092e2b455",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/a101444fdd04a31885a6a9ea2a85608e5d541617"
+        },
+        "date": 1658305257920,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test_without_numba.py::test_tz_without_numba",
+            "value": 47.44850369797391,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0012478317586530165",
+            "extra": "mean: 21.075480195652638 msec\nrounds: 46"
+          }
+        ]
+      }
+    ]
+  }
+}
