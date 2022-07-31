e6fdf8ef4113ad39deee6bbf9814a0b02e0b095e
ringsaturn <ringsaturn.me@gmail.com>
2022-07-31 23:59:38 +0800

    add tzfpy

--- tz_test.py
+++ tz_test.py
@@ -1,5 +1,5 @@
 from timezonefinder import TimezoneFinder
-
+from tzfpy import get_tz
 
 tf = TimezoneFinder()
 
@@ -12,3 +12,13 @@ def _test_tz_with_numba():
 
 def test_tz_with_numba(benchmark):
     benchmark(_test_tz_with_numba)
+
+
+def _test_tzfpy():
+    _ = get_tz(lng=13.358, lat=52.5061)
+    _ = get_tz(lng=116, lat=39)
+    _ = get_tz(lng=0.1276, lat=51.5073)
+
+
+def test_tzfpy(benchmark):
+    benchmark(_test_tzfpy)
