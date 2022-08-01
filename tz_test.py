0bcabf2c4b271fd6e6226bcf6acef978b21ecc91
ringsaturn <ringsaturn.me@gmail.com>
2022-08-01 16:57:55 +0800

    fix python benchmark

--- tz_test.py
+++ tz_test.py
@@ -2,48 +2,49 @@ import numpy as np
 from timezonefinder import TimezoneFinder
 from tzfpy import get_tz
 
-tf = TimezoneFinder()
+tf = TimezoneFinder(in_memory=True)
 
-
-def _test_tz_with_numba():
-    _ = tf.timezone_at(lng=13.358, lat=52.5061)
-    _ = tf.timezone_at(lng=116, lat=39)
-    _ = tf.timezone_at(lng=0.1276, lat=51.5073)
-
-
-def test_tz_with_numba(benchmark):
-    benchmark(_test_tz_with_numba)
-
-
-def _test_tzfpy():
-    _ = get_tz(lng=13.358, lat=52.5061)
-    _ = get_tz(lng=116, lat=39)
-    _ = get_tz(lng=0.1276, lat=51.5073)
-
-
-def test_tzfpy(benchmark):
-    benchmark(_test_tzfpy)
-
-
-lat_ranges = np.arange(-60, 60, 0.5)
 lng_ranges = np.arange(-180, 180, 0.5)
+lat_ranges = np.arange(-60, 60, 0.5)
 
 
-def _test_tz_global():
+def _test_timezonefinder_iter_global():
     for lat in lat_ranges:
         for lng in lng_ranges:
             _ = tf.timezone_at(lng=lng, lat=lat)
 
 
-def test_tz_global(benchmark):
-    benchmark(_test_tz_global)
+def test_timezonefinder_iter_global(benchmark):
+    benchmark(_test_timezonefinder_iter_global)
 
 
-def _test_tzfpy_global():
+def _test_tzfpy_iter_global():
     for lat in lat_ranges:
         for lng in lng_ranges:
             _ = get_tz(lng, lat)
 
 
-def test_tzfpy_global(benchmark):
-    benchmark(_test_tzfpy_global)
+def test_tzfpy_iter_global(benchmark):
+    benchmark(_test_tzfpy_iter_global)
+
+
+def random_point():
+    return np.random.choice(lng_ranges), np.random.choice(lat_ranges)
+
+
+def _test_timezonefinder_random():
+    lng, lat = random_point()
+    _ = tf.timezone_at(lng=lng, lat=lat)
+
+
+def test_timezonefinder_random(benchmark):
+    benchmark(_test_timezonefinder_random)
+
+
+def _test_tzfpy_random():
+    lng, lat = random_point()
+    _ = get_tz(lng, lat)
+
+
+def test_tzfpy_random(benchmark):
+    benchmark(_test_tzfpy_random)
