37f176c61fdc4de06ff9f3d612f8eb5b1b6c1de7
ringsaturn <ringsaturn.me@gmail.com>
2022-08-01 17:21:16 +0800

    timezonefinder without numba only benchmark random

--- tz_test_without_numba.py
+++ tz_test_without_numba.py
@@ -1,31 +1,10 @@
 import numpy as np
 from timezonefinder import TimezoneFinder
-from tzfpy import get_tz
 
 tf = TimezoneFinder(in_memory=True)
 
 lng_ranges = np.arange(-180, 180, 0.5)
 lat_ranges = np.arange(-60, 60, 0.5)
-
-
-def _test_timezonefinder_without_numbaiter_global():
-    for lat in lat_ranges:
-        for lng in lng_ranges:
-            _ = tf.timezone_at(lng=lng, lat=lat)
-
-
-def test_timezonefinder_without_numbaiter_global(benchmark):
-    benchmark(_test_timezonefinder_without_numbaiter_global)
-
-
-def _test_tzfpy_iter_global():
-    for lat in lat_ranges:
-        for lng in lng_ranges:
-            _ = get_tz(lng, lat)
-
-
-def test_tzfpy_iter_global(benchmark):
-    benchmark(_test_tzfpy_iter_global)
 
 
 def random_point():
@@ -39,12 +18,3 @@ def _test_timezonefinder_without_numbarandom():
 
 def test_timezonefinder_without_numbarandom(benchmark):
     benchmark(_test_timezonefinder_without_numbarandom)
-
-
-def _test_tzfpy_random():
-    lng, lat = random_point()
-    _ = get_tz(lng, lat)
-
-
-def test_tzfpy_random(benchmark):
-    benchmark(_test_tzfpy_random)
