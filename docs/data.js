a9451850200f80776496231393bc13f8e4d33ea6
github-action-benchmark <github@users.noreply.github.com>
2022-11-16 17:00:51 +0000

    add Python Library Benchmark (pytest) benchmark result for f2c31426d35d63147067cf90a5befec9911c5b50

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668618015292,
+  "lastUpdate": 1668618051299,
   "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
   "entries": {
     "Python Library Benchmark": [
@@ -12236,6 +12236,79 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00008987835064379346",
             "extra": "mean: 85.8071666661536 usec\nrounds: 54"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2c31426d35d63147067cf90a5befec9911c5b50",
+          "message": "Bump github.com/ringsaturn/tzf from 0.9.2 to 0.10.1 (#46)\n\nBumps [github.com/ringsaturn/tzf](https://github.com/ringsaturn/tzf) from 0.9.2 to 0.10.1.\r\n- [Release notes](https://github.com/ringsaturn/tzf/releases)\r\n- [Commits](https://github.com/ringsaturn/tzf/compare/v0.9.2...v0.10.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/ringsaturn/tzf\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-11-17T00:58:28+08:00",
+          "tree_id": "dbd31fbc2fb8dbfec6be81f47f2fdae76d4a25ec",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/f2c31426d35d63147067cf90a5befec9911c5b50"
+        },
+        "date": 1668618049968,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test.py::test_timezonefinder_iter_global",
+            "value": 0.1157340205537503,
+            "unit": "iter/sec",
+            "range": "stddev: 0.11188480167385904",
+            "extra": "mean: 8.640501688400002 sec\nrounds: 5"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_iter_global",
+            "value": 0.3571110616574482,
+            "unit": "iter/sec",
+            "range": "stddev: 0.052705762561173014",
+            "extra": "mean: 2.80024929880002 sec\nrounds: 5"
+          },
+          {
+            "name": "tz_test.py::test_timezonefinder_random_certain",
+            "value": 2289.173674933446,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00029959885458590404",
+            "extra": "mean: 436.83885191850874 usec\nrounds: 1668"
+          },
+          {
+            "name": "tz_test.py::test_timezonefinder_random",
+            "value": 8139.228698141182,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0002186168017215848",
+            "extra": "mean: 122.86176455864641 usec\nrounds: 1992"
+          },
+          {
+            "name": "tz_test.py::test_timezonefinder_random_city",
+            "value": 10573.608681325668,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0001284861934028528",
+            "extra": "mean: 94.57509069407179 usec\nrounds: 11026"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_random",
+            "value": 13067.454896298608,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00003777083107999742",
+            "extra": "mean: 76.5259959139597 usec\nrounds: 3426"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_random_cities",
+            "value": 43692.28180433082,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000024239388160805625",
+            "extra": "mean: 22.887337504558502 usec\nrounds: 2557"
           }
         ]
       }
