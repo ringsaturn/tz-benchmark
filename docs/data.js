d6cfb6a3dec873f0a91a81128e7edd8ba36217e0
github-action-benchmark <github@users.noreply.github.com>
2022-11-16 16:59:07 +0000

    add Python Library Benchmark (pytest) benchmark result for f2c31426d35d63147067cf90a5befec9911c5b50

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1668562457418,
+  "lastUpdate": 1668617947566,
   "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
   "entries": {
     "Python Library Benchmark": [
@@ -12205,6 +12205,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000007412823939770591",
             "extra": "mean: 19.695020260270088 usec\nrounds: 6762"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f2c31426d35d63147067cf90a5befec9911c5b50",
+          "message": "Bump github.com/ringsaturn/tzf from 0.9.2 to 0.10.1 (#46)\n\nBumps [github.com/ringsaturn/tzf](https://github.com/ringsaturn/tzf) from 0.9.2 to 0.10.1.\r\n- [Release notes](https://github.com/ringsaturn/tzf/releases)\r\n- [Commits](https://github.com/ringsaturn/tzf/compare/v0.9.2...v0.10.1)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: github.com/ringsaturn/tzf\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-minor\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2022-11-17T00:58:28+08:00",
+          "tree_id": "dbd31fbc2fb8dbfec6be81f47f2fdae76d4a25ec",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/f2c31426d35d63147067cf90a5befec9911c5b50"
+        },
+        "date": 1668617946773,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test_without_numba.py::test_timezonefinder_without_numbarandom",
+            "value": 11654.03822143037,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00008987835064379346",
+            "extra": "mean: 85.8071666661536 usec\nrounds: 54"
           }
         ]
       }
