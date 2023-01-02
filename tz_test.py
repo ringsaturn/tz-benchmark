795cdce7ef02f320113772377f259f4dc7aa9e1c
ringsaturn <ringsaturn.me@gmail.com>
2023-01-02 14:11:51 +0800

    replace city data (#67)

--- tz_test.py
+++ tz_test.py
@@ -1,25 +1,15 @@
-import json
-import random
-
+import citiespy
 import numpy as np
 from timezonefinder import TimezoneFinder
 from tzfpy import get_tz
-from tzfrspy import get_tz as rs_get_tz
-
-# Rust use lazy init
-_ = rs_get_tz(116.3883, 39.9289)
 
 tf = TimezoneFinder(in_memory=True)
 
 lng_ranges = np.arange(-180, 180, 0.5)
 lat_ranges = np.arange(-60, 60, 0.5)
 
-
-cities = []
-with open("cities.json") as f:
-    raw_cities = json.loads(f.read())
-for rawcity in raw_cities:
-    cities.append([float(rawcity["lng"]), float(rawcity["lat"])])
+_ = get_tz(116, 39)
+_ = citiespy.random_city()
 
 
 def _test_timezonefinder_iter_global():
@@ -47,8 +37,8 @@ def random_point():
 
 
 def random_city():
-    index = random.randint(0, len(cities)-1)
-    return cities[index]
+    city = citiespy.random_city()
+    return city.lng, city.lat
 
 
 def _test_timezonefinder_random_certain():
@@ -94,21 +84,3 @@ def _test_tzfpy_random_city():
 
 def test_tzfpy_random_cities(benchmark):
     benchmark(_test_tzfpy_random_city)
-
-
-def _test_tzfrspy_random():
-    lng, lat = random_point()
-    _ = rs_get_tz(lng, lat)
-
-
-def test_tzfrspy_random(benchmark):
-    benchmark(_test_tzfrspy_random)
-
-
-def _test_tzfrspy_random_city():
-    lng, lat = random_city()
-    _ = rs_get_tz(lng, lat)
-
-
-def test_tzfrspy_random_cities(benchmark):
-    benchmark(_test_tzfrspy_random_city)
