9d0a532fc7ece699ecefa6fabbcdc0f74a574307
ringsaturn <ringsaturn.me@gmail.com>
2023-08-01 22:41:59 +0800

    Add tz-search (#115)

---
+++ benches/benches.rs
@@ -0,0 +1,45 @@
+#![feature(test)]
+extern crate test;
+
+#[cfg(test)]
+mod benches_tz_crates {
+    use test::Bencher;
+    use cities_json;
+    use tz_search;
+    use tzf_rs::{DefaultFinder, Finder, FuzzyFinder};
+
+    #[bench]
+    fn bench_tz_search_lookup_random_city(b: &mut Bencher) {
+        b.iter(|| {
+            let city = cities_json::get_random_cities();
+            let _ = tz_search::lookup(city.lat, city.lng);
+        });
+    }
+
+    #[bench]
+    fn bench_tzf_fuzzy_finder_random_city(b: &mut Bencher) {
+        let finder: FuzzyFinder = FuzzyFinder::new();
+        b.iter(|| {
+            let city = cities_json::get_random_cities();
+            let _ = finder.get_tz_name(city.lng, city.lat);
+        });
+    }
+
+    #[bench]
+    fn bench_tzf_default_finder_random_city(b: &mut Bencher) {
+        let finder: DefaultFinder = DefaultFinder::new();
+        b.iter(|| {
+            let city = cities_json::get_random_cities();
+            let _ = finder.get_tz_name(city.lng, city.lat);
+        });
+    }
+
+    #[bench]
+    fn bench_tzf_finder_random_city(b: &mut Bencher) {
+        let finder: Finder = Finder::new();
+        b.iter(|| {
+            let city = cities_json::get_random_cities();
+            let _ = finder.get_tz_name(city.lng, city.lat);
+        });
+    }
+}
