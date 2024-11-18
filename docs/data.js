8113fed6698db91c349fd68633cc0aaa4f22a27f
github-action-benchmark <github@users.noreply.github.com>
2024-11-18 01:33:21 +0000

    add Python Library Benchmark (pytest) benchmark result for ccc9b34cf8b7e725907b267c12376964ec4eabcf

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731288966097,
+  "lastUpdate": 1731893601800,
   "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
   "entries": {
     "Python Library Benchmark": [
@@ -4654,6 +4654,42 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000032932059602717257",
             "extra": "mean: 3.0495901800748744 usec\nrounds: 73015"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "ccc9b34cf8b7e725907b267c12376964ec4eabcf",
+          "message": "Bump cities-json from 0.6.3 to 0.6.6 in /rust in the dependencies group (#169)\n\nBumps the dependencies group in /rust with 1 update: [cities-json](https://github.com/ringsaturn/go-cities.json).\r\n\r\n\r\nUpdates `cities-json` from 0.6.3 to 0.6.6\r\n- [Release notes](https://github.com/ringsaturn/go-cities.json/releases)\r\n- [Commits](https://github.com/ringsaturn/go-cities.json/compare/v0.6.3...v0.6.6)\r\n\r\n---\r\nupdated-dependencies:\r\n- dependency-name: cities-json\r\n  dependency-type: direct:production\r\n  update-type: version-update:semver-patch\r\n  dependency-group: dependencies\r\n...\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-11-02T01:14:44Z",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/ccc9b34cf8b7e725907b267c12376964ec4eabcf"
+        },
+        "date": 1731893599456,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test.py::test_timezonefinder_random_city",
+            "value": 32182.073073384716,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00012281228756305318",
+            "extra": "mean: 31.07320021676982 usec\nrounds: 11997"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_random_cities",
+            "value": 323663.9337808937,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000003442643836403503",
+            "extra": "mean: 3.089624439518047 usec\nrounds: 41711"
           }
         ]
       }
