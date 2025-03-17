781c246a03eb60dd2164f98741cddc5edfaf5101
github-action-benchmark <github@users.noreply.github.com>
2025-03-17 01:33:05 +0000

    add Python Library Benchmark (pytest) benchmark result for f74676500fcb3c73ee3ee2b3513ebe4b5b8bb113

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1741570497064,
+  "lastUpdate": 1742175185728,
   "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
   "entries": {
     "Python Library Benchmark": [
@@ -5798,6 +5798,42 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000003284827280598177",
             "extra": "mean: 2.980341179891252 usec\nrounds: 49194"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f74676500fcb3c73ee3ee2b3513ebe4b5b8bb113",
+          "message": "Bump github.com/albertyw/localtimezone/v3 (#183)\n\nBumps the dependencies group in /go with 1 update: [github.com/albertyw/localtimezone/v3](https://github.com/albertyw/localtimezone).\n\n\nUpdates `github.com/albertyw/localtimezone/v3` from 3.1.7 to 3.1.8\n- [Changelog](https://github.com/albertyw/localtimezone/blob/master/CHANGELOG.md)\n- [Commits](https://github.com/albertyw/localtimezone/compare/v3.1.7...v3.1.8)\n\n---\nupdated-dependencies:\n- dependency-name: github.com/albertyw/localtimezone/v3\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n  dependency-group: dependencies\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-03-07T11:49:42Z",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/f74676500fcb3c73ee3ee2b3513ebe4b5b8bb113"
+        },
+        "date": 1742175183741,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test.py::test_timezonefinder_random_city",
+            "value": 30710.339121922443,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0006120491165805338",
+            "extra": "mean: 32.562323588480155 usec\nrounds: 9475"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_random_cities",
+            "value": 317916.3257811694,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000034975903109762914",
+            "extra": "mean: 3.14548174757256 usec\nrounds: 72073"
           }
         ]
       }
