9f24aa2d71509497130d36bae2ec0d0692b9ecee
github-action-benchmark <github@users.noreply.github.com>
2025-04-07 01:33:20 +0000

    add Python Library Benchmark (pytest) benchmark result for 250f781c3ee0dbe9b5c1018ae2522c254898c5d4

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743669099162,
+  "lastUpdate": 1743989600471,
   "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
   "entries": {
     "Python Library Benchmark": [
@@ -6020,6 +6020,42 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0000058672567760704296",
             "extra": "mean: 4.5745720063566715 usec\nrounds: 59592"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "250f781c3ee0dbe9b5c1018ae2522c254898c5d4",
+          "message": "Bump spatialtime from 0.2.1 to 0.2.2 in /rust in the dependencies group (#184)\n\nBumps the dependencies group in /rust with 1 update: [spatialtime](https://github.com/moranbw/spatialtime).\n\n\nUpdates `spatialtime` from 0.2.1 to 0.2.2\n- [Commits](https://github.com/moranbw/spatialtime/commits)\n\n---\nupdated-dependencies:\n- dependency-name: spatialtime\n  dependency-version: 0.2.2\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n  dependency-group: dependencies\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-04-03T08:27:45Z",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/250f781c3ee0dbe9b5c1018ae2522c254898c5d4"
+        },
+        "date": 1743989598488,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test.py::test_timezonefinder_random_city",
+            "value": 32199.31843366195,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005063691790560768",
+            "extra": "mean: 31.056557984611736 usec\nrounds: 13219"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_random_cities",
+            "value": 220264.95110483092,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000005878595168917289",
+            "extra": "mean: 4.539986933845271 usec\nrounds: 69799"
           }
         ]
       }
