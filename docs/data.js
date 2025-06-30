316c4fc2afa46986d3f94e442f9259d816c4115d
github-action-benchmark <github@users.noreply.github.com>
2025-06-30 01:34:27 +0000

    add Python Library Benchmark (pytest) benchmark result for f307965164630edcf4fd2692d3167e2dde4825bd

--- docs/data.js
+++ docs/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1750642684676,
+  "lastUpdate": 1751247267494,
   "repoUrl": "https://github.com/ringsaturn/tz-benchmark",
   "entries": {
     "Python Library Benchmark": [
@@ -6566,6 +6566,42 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.000005923715246610512",
             "extra": "mean: 4.518928692377657 usec\nrounds: 65028"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]",
+            "email": "49699333+dependabot[bot]@users.noreply.github.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "f307965164630edcf4fd2692d3167e2dde4825bd",
+          "message": "Bump citiespy from 0.6.8 to 0.6.11 in /python in the dependencies group (#187)\n\nBumps the dependencies group in /python with 1 update: [citiespy](https://github.com/ringsaturn/go-cities.json).\n\n\nUpdates `citiespy` from 0.6.8 to 0.6.11\n- [Release notes](https://github.com/ringsaturn/go-cities.json/releases)\n- [Commits](https://github.com/ringsaturn/go-cities.json/compare/v0.6.8...v0.6.11)\n\n---\nupdated-dependencies:\n- dependency-name: citiespy\n  dependency-version: 0.6.11\n  dependency-type: direct:production\n  update-type: version-update:semver-patch\n  dependency-group: dependencies\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2025-05-01T22:47:20Z",
+          "url": "https://github.com/ringsaturn/tz-benchmark/commit/f307965164630edcf4fd2692d3167e2dde4825bd"
+        },
+        "date": 1751247265530,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tz_test.py::test_timezonefinder_random_city",
+            "value": 33270.29611951921,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000502633798869831",
+            "extra": "mean: 30.056840985353123 usec\nrounds: 12477"
+          },
+          {
+            "name": "tz_test.py::test_tzfpy_random_cities",
+            "value": 214281.20533189716,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000060811384313349205",
+            "extra": "mean: 4.666764863727148 usec\nrounds: 15574"
           }
         ]
       }
