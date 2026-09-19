6c05c239e4dbf55bf94ae07d042d6a42f790eb08
Albert Wang <git@albertyw.com>
2026-09-19 16:24:43 -0700

    Update localtimezone from v3 to v4

--- go/internal/cmd/memory/candidates/localtimezone/main.go
+++ go/internal/cmd/memory/candidates/localtimezone/main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"github.com/albertyw/localtimezone/v3"
+	"github.com/albertyw/localtimezone/v4"
 	"github.com/ringsaturn/tz-benchmark/go/internal/memprobe"
 )
 
 func main() {
 	memprobe.Run("albertyw/localtimezone", func() func(lng, lat float64) string {
-		ltz, err := localtimezone.NewLocalTimeZone()
-		if err != nil {
-			panic(err)
-		}
+		ltz := localtimezone.NewLocalTimeZone()
 		return func(lng, lat float64) string {
 			zones, err := ltz.GetZone(localtimezone.Point{Lon: lng, Lat: lat})
 			if err != nil || len(zones) == 0 {
